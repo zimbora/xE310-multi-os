63bf1f04a74bc9ac3db4f63ab785c63a383d9577
zimbora <lucas.ua.eet@gmail.com>
2026-06-01 16:32:53 +0100

    fix: set cops for 210s of timeout

--- src/xe310.cpp
+++ src/xe310.cpp
@@ -608,13 +608,13 @@ ModemStatus xE310::set_operator_manual(const std::string& oper, RadioTech tech) 
     } else {
         auto cmd = "AT+COPS=1,2,\"" + oper + "\"," + std::to_string(static_cast<int>(tech));
 
-        return controller_.send_raw(cmd, response, 45000); // manual registration can take a long time, allow up to 30s
+        return controller_.send_raw(cmd, response, 210000); // manual registration can take a long time, allow up to 30s
     }
 }
 
 ModemStatus xE310::set_operator_auto() {
     AtResponse response;
-    return controller_.send_raw("AT+COPS=0", response,45000); // registration can take a long time, allow up to 30s 
+    return controller_.send_raw("AT+COPS=0", response,210000); // registration can take a long time, allow up to 30s 
 }
 
 ModemStatus xE310::get_operator(std::string& oper) {
