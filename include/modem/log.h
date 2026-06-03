89591a36ba0e8fc0c180efa753c56b80946fbc74
zimbora <lucas.ua.eet@gmail.com>
2026-06-03 15:44:03 +0100

    feat: adds timestamp to logs if not zephyr

--- include/modem/log.h
+++ include/modem/log.h
@@ -15,13 +15,34 @@
 #else
 
 #include <cstdio>
+#include <ctime>
+#include <chrono>
+
+namespace modem_log_detail {
+inline const char* timestamp() {
+    using clock = std::chrono::system_clock;
+    static char buf[24];
+    auto now   = clock::now();
+    auto ms    = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+    std::time_t t = clock::to_time_t(now);
+    struct tm tm_buf;
+#if defined(_WIN32)
+    localtime_s(&tm_buf, &t);
+#else
+    localtime_r(&t, &tm_buf);
+#endif
+    std::snprintf(buf, sizeof(buf), "%02d:%02d:%02d.%03d",
+                  tm_buf.tm_hour, tm_buf.tm_min, tm_buf.tm_sec, (int)ms.count());
+    return buf;
+}
+} // namespace modem_log_detail
 
 #define MODEM_LOG_MODULE_REGISTER(name)
 #define MODEM_LOG_MODULE_DECLARE(name)
 
-#define MODEM_LOG_DBG(...) do { std::fprintf(stdout, "[DBG] " __VA_ARGS__); std::fprintf(stdout, "\n"); } while (0)
-#define MODEM_LOG_INF(...) do { std::fprintf(stdout, "[INF] " __VA_ARGS__); std::fprintf(stdout, "\n"); } while (0)
-#define MODEM_LOG_WRN(...) do { std::fprintf(stderr, "[WRN] " __VA_ARGS__); std::fprintf(stderr, "\n"); } while (0)
-#define MODEM_LOG_ERR(...) do { std::fprintf(stderr, "[ERR] " __VA_ARGS__); std::fprintf(stderr, "\n"); } while (0)
+#define MODEM_LOG_DBG(...) do { std::fprintf(stdout, "[%s][DBG] ", modem_log_detail::timestamp()); std::fprintf(stdout, __VA_ARGS__); std::fprintf(stdout, "\n"); } while (0)
+#define MODEM_LOG_INF(...) do { std::fprintf(stdout, "[%s][INF] ", modem_log_detail::timestamp()); std::fprintf(stdout, __VA_ARGS__); std::fprintf(stdout, "\n"); } while (0)
+#define MODEM_LOG_WRN(...) do { std::fprintf(stderr, "[%s][WRN] ", modem_log_detail::timestamp()); std::fprintf(stderr, __VA_ARGS__); std::fprintf(stderr, "\n"); } while (0)
+#define MODEM_LOG_ERR(...) do { std::fprintf(stderr, "[%s][ERR] ", modem_log_detail::timestamp()); std::fprintf(stderr, __VA_ARGS__); std::fprintf(stderr, "\n"); } while (0)
 
 #endif
