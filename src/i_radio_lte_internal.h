db4c9a8ff56a85cfa9891fd70e11859fe45a3665
zimbora <lucas.ua.eet@gmail.com>
2026-07-14 15:31:29 +0100

    style: fix ci clang

--- src/i_radio_lte_internal.h
+++ src/i_radio_lte_internal.h
@@ -7,6 +7,6 @@ namespace modem {
 class NetworkLte;
 
 /// Process pending radio LTE requests from channels and dispatch them on the network thread.
-void process_radio_requests(RadioLteChannels& channels, NetworkLte& network);
+void process_radio_requests(RadioLteChannels& channels, NetworkLte& radio);
 
 } // namespace modem
